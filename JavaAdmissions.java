d1b379b9dcbea4b49042bea323b4e91f54fb48b0
KeltonPygott <42286001+KeltonPygott@users.noreply.github.com>
2020-09-28 10:59:23 -0600

    Create JavaAdmissions.java

---
+++ JavaAdmissions.java
@@ -0,0 +1,40 @@
+import java.util.scanner
+
+public class JavaAdmissions {
+
+  public main static void (String[] args) {
+
+    Scanner SAT = new Scanner(System.in);
+    System.out.println("Enter SAT Scores");
+    
+    String SATscore = SATscore.nextLine();
+    
+    Scanner ClassRank = new Scanner(System.in);
+    System.out.println("Enter your class rank");
+    
+    String ClassRank = classRank.nextLine();
+    
+    Scanner Awards = new Scanner(System.in);
+    System.out.println("Enter your Awards");
+    
+    String Awards = awards.nextLine();
+    
+    Scanner Area = new Scanner(System.in);
+    System.out.println("Inpout your State")
+    
+    String area = area.nextLine();
+   
+   
+   // GATHERING INFORMATION THROUGH A SCANNER (SAT Scores, Class Rank, Awards, and Location
+   // Variables will be used to store data to see if student gets accepted. 
+   
+   
+    
+   
+  
+    
+
+}
+  }
+
+
